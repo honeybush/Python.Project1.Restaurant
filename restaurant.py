de909367370010a7b5600e56928f5980bec26437
Sequoia Hummingbird <sequoia_hummingbird@yahoo.com>
2014-07-03 19:02:18 +0800

    Restaurant 1.1
    You can now order specific food objects from the menu

--- restaurant.py
+++ restaurant.py
@@ -2,23 +2,51 @@ class Food(): #model
 	def __init__(self):
 		self.name = ""
 
+class Menu():
+	def __init__(self):
+		self.food = []
+		f = open("recipes.txt", "r")
+		for line in f:
+			temp = Food()
+			if line[len(line)-1] == "\n":
+				temp.name = line[:len(line)-1]
+			else:
+				temp.name = line
+			print temp.name
+			self.food.append(temp)
+		f.close()
+	def giveMenu(self):
+		return self.food
+
 class Waiter(): #view
 	def __init__(self):
 		self.order = ""
 	def getOrder(self):
 		order = raw_input("What is your order? ")
 		return order
-	def giveFood(self, food):
-		print "Here is your", food + ". Enjoy your meal!"
+	def giveFood(self, avai, food):
+		if avai == True:
+			print "Here is your", food + ". Enjoy your meal!"
+		else:
+			print "Sorry, the food you ordered is not available."
 
 class Chef(): #control
 	def __init__(self):
-		self.food = Food()
+		self.menu = Menu()
+		self.foodlist = self.menu.giveMenu()
 		self.waiter = Waiter()
-		self.cooking()
-	def cooking(self):
-		food = self.waiter.getOrder()
-		self.food.name = food
-		self.waiter.giveFood(self.food.name)
+		self.avai = False
+		self.confirmOrder()
+	def confirmOrder(self):
+		order = self.waiter.getOrder()
+		order = order.lower()
+		for food in self.foodlist:
+			if food.name == order:
+				self.avai = True
+				self.cooking(food)
+		if self.avai == False:
+			self.waiter.giveFood(self.avai, "n/a")
+	def cooking(self, food):
+		self.waiter.giveFood(self.avai, food.name)
 
 start = Chef()
